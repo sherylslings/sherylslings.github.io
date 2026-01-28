7ca32f78ead9cd00ba054934c466a2e7ed3bf5b3
Gaurav Sharma <gauravsharma@Gauravs-MacBook-Pro.local>
2026-01-28 16:55:10 +0530

    Set base to / for org GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/", // root because site will be at https://sherylslings.github.io
 }));
